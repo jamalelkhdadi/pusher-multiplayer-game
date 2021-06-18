ed20866e35a77dff7badbbd23f8ca02d28e5c356
jamalelkhdadi <jamal.elkhdadi@outlook.com>
2021-06-18 15:12:07 -0800

    kk

--- app.py
+++ app.py
@@ -4,38 +4,42 @@ from pusher import pusher
 app = Flask(__name__)
 
 pusher = pusher_client = pusher.Pusher(
-  app_id='PUSHER_APP_ID',
-  key='PUSHER_APP_KEY',
-  secret='PUSHER_APP_SECRET',
-  cluster='PUSHER_APP_CLUSTER',
-  ssl=True
+    app_id='1081696',
+    key='6106d58aab6449a47cb6',
+    secret='9f2ccdbdebbc222aebd7',
+    cluster='mt1',
+    ssl=True
 )
 
 name = ''
 
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/play')
 def play():
-  global name
-  name = request.args.get('username')
-  return render_template('play.html')
+    global name
+    name = request.args.get('username')
+    return render_template('play.html')
+
 
 @app.route("/pusher/auth", methods=['POST'])
 def pusher_authentication():
-  auth = pusher.authenticate(
-    channel=request.form['channel_name'],
-    socket_id=request.form['socket_id'],
-    custom_data={
-      u'user_id': name,
-      u'user_info': {
-        u'role': u'player'
-      }
-    }
-  )
-  return json.dumps(auth)
+    auth = pusher.authenticate(
+        channel=request.form['channel_name'],
+        socket_id=request.form['socket_id'],
+        custom_data={
+            u'user_id': name,
+            u'user_info': {
+                u'role': u'player'
+            }
+        }
+    )
+    return json.dumps(auth)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
